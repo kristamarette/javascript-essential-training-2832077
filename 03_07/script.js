f267413e3978306f6af9efbbfe73032085b8e9ad
kristamarette <55501269+kristamarette@users.noreply.github.com>
2021-11-15 09:17:12 -0600

    practice through 03_07

---
+++ 03_07/script.js
@@ -0,0 +1,15 @@
+const cup = {
+    name : "a cup of coffee",
+    color : '#fff',
+    pattern : null,
+    handle : true,
+    temperature : "hot",
+    contains : {
+        coffee : 80,
+        almondMilk : 20,
+    }
+}
+
+console.log("My favorite morning beverage is:", cup );
+console.log(cup.name);
+console.log("The first thing I make in the morning", cup.name);
